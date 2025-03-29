c8583a48184822cc6d668afe298acc15d3a900b3
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2025-03-29 00:36:51 -0400

    remove write dynamic ip
    fixed IPv6 addresses removes the need for dynamically assigned IPv4 addresses

--- mc_server_watcher.py
+++ mc_server_watcher.py
@@ -43,35 +43,6 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print("Logged on as", self.user)
 
-    @staticmethod
-    async def write_dynamic_ip(instance_name, channel):
-        for i in range(10):
-            await asyncio.sleep(1)
-
-            instance = describe_instance(instance_name)
-            if instance is None:
-                return
-
-            network_interfaces = instance["NetworkInterfaces"]
-            if len(network_interfaces) != 1:
-                return
-
-            network_interface = network_interfaces[0]
-            if "Association" not in network_interface:
-                continue
-
-            association = network_interface["Association"]
-            if "PublicIp" not in association:
-                continue
-
-            stable = False
-            if "IpOwnerId" in association and association["IpOwnerId"] != "amazon":
-                stable = True
-
-            if not stable:
-                await channel.send(f"IP for {instance_name}: \n```{association['PublicIp']}```")
-            return
-
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -111,10 +82,6 @@ class MyClient(discord.Client):
                 return
 
             await message.channel.send(f"Started {instance_name}.")
-
-            self.write_dynamic_ip_tasks[instance_name] = asyncio.create_task(
-                self.write_dynamic_ip(instance_name, message.channel)
-            )
 
         elif command == "ip":
             instance = describe_instance(instance_name)
